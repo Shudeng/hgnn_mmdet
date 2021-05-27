c70cff3612b9408e87969e2bdf0a65180c7889ad
shudeng <wsd19@mails.tsinghua.edu.cn>
2021-05-27 13:47:58 +0800

    add baseline

--- train.sh
+++ train.sh
@@ -12,7 +12,7 @@ export PYTHONPATH=$PWD:$PYTHONPATH
 
 #CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port=29709 ./tools/train.py configs/votenet/votenet_16x8_sunrgbd-3d-10class.py --launcher pytorch --work-dir gnn --resume-from ./gnn/latest.pth
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=4 --master_port=29707 ./tools/train.py configs/votenet/votenet_16x8_sunrgbd-3d-10class.py --launcher pytorch --work-dir knn_gnn
+CUDA_VISIBLE_DEVICES=6,7 python -m torch.distributed.launch --nproc_per_node=2 --master_port=29707 ./tools/train.py configs/votenet/votenet_16x8_sunrgbd-3d-10class.py --launcher pytorch --work-dir knn_gnn
 
 #CUDA_LAUNCH_BLOCKING=1 python train_mmdet.py configs/votenet/votenet_16x8_sunrgbd-3d-10class.py
 
